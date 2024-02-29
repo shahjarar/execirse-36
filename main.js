5d473c8f7f4096ca5aa5d2e2f557871d7878d6ea
shahjarar <160387866+shahjarar@users.noreply.github.com>
2024-02-29 18:20:45 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+// Task 36
+// // T-shirt: write a function called make_shirt() that  accept a size and the test of a message that should be printed on the shirt,
+// // the function should print a sentence summarizing the size of the shirt and the message printed on it . call the function.
+function makeShirt(size, text) {
+    console.log('/n you order a $(size) shirt that says $(text)');
+}
+makeShirt('large', '" i love typescript"');
+makeShirt('meduim', '"i need a big shirt"');
+console.log("i love typescripts");
+console.log("i need a big shirt");
